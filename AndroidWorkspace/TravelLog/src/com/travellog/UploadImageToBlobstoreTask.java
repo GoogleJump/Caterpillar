3723a9dca8367e4abc53cea7038f69468e80a427
Shayan Patel <shayanpatel@Shayans-MacBook-Pro.local>
2014-06-23 01:05:25 -0700

    added upload image code

--- AndroidWorkspace/TravelLog/src/com/travellog/UploadImageToBlobstoreTask.java
+++ AndroidWorkspace/TravelLog/src/com/travellog/UploadImageToBlobstoreTask.java
@@ -12,6 +12,7 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.mime.MultipartEntity;
 import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.HttpConnectionParams;
 
 import com.google.api.client.http.HttpResponse;
 import com.google.appengine.labs.repackaged.org.json.JSONException;
@@ -22,62 +23,66 @@ import android.util.Log;
 
 public class UploadImageToBlobstoreTask extends AsyncTask {
 
-	@Override
-	protected Object doInBackground(Object... params) {
-		HttpClient httpClient = new DefaultHttpClient();    
-		//This will invoke "ImgUpload servlet           
-		HttpGet httpGet = new HttpGet("http://10.0.0.2:8888/ImgUpload.java"); 
-		org.apache.http.HttpResponse response;
-		try {
-			response = httpClient.execute(httpGet);
-			HttpEntity urlEntity = response.getEntity();
-			InputStream in = urlEntity.getContent();
-			String str = "";
-			while (true) {
-			    int ch = in.read();
-			    if (ch == -1)
-			        break;
-			    str += (char) ch;
-			}
-			
-			Log.v("str", str);
-			
-//			HttpPost httppost = new HttpPost(str);
-//			File f = new File((String) params[1]);
-//			FileBody fileBody = new FileBody(f);
-//			MultipartEntity reqEntity = new MultipartEntity();
-//			reqEntity.addPart("file", fileBody);
-//			httppost.setEntity(reqEntity);
-//			response = httpClient.execute(httppost); //Here "uploaded" servlet is automatically       invoked
-//			urlEntity = response.getEntity(); //Response will be returned by "uploaded" servlet in JSON format
-//			in = urlEntity.getContent();
-//			str = "";
-//			while (true) {
-//			    int ch = in.read();
-//			    if (ch == -1)
-//			        break;
-//			    str += (char) ch;
-//			}
-//			JSONObject resultJson;
-//			try {
-//				resultJson = new JSONObject(str);
-//				String blobKey = resultJson.getString("blobKey");
-//				String servingUrl = resultJson.getString("servingUrl");
-//				Log.v("blobkey", blobKey);
-//				Log.v("servingurl", servingUrl);
-//			} catch (JSONException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-//			
-		} catch (ClientProtocolException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return null;
-	}
+    @Override
+    protected Object doInBackground(Object... params) {
+        try {
+            HttpClient httpclient = new DefaultHttpClient();    
+            HttpConnectionParams.setConnectionTimeout(httpclient.getParams(), 10000);
+            HttpGet httpGet = new HttpGet("http://travellogwebapp.appspot.com/blob/getuploadurl");
+            
+            org.apache.http.HttpResponse response = httpclient.execute(httpGet);
+            HttpEntity urlEntity = response.getEntity();
+            InputStream in = urlEntity.getContent();
+            String str = "";
+            while (true) {
+                int ch = in.read();
+                if (ch == -1)
+                    break;
+                str += (char) ch;
+            }
+                        
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost(str);
+
+            FileBody fileBody  = new FileBody(new File((String) params[1]));
+            MultipartEntity reqEntity = new MultipartEntity();
+
+            reqEntity.addPart("file", fileBody);
+
+            httppost.setEntity(reqEntity);
+            org.apache.http.HttpResponse responseTwo = httpClient.execute(httppost);
+            HttpEntity urlEntity2 = responseTwo.getEntity(); //Response will be returned by "uploaded" servlet in JSON format
+            InputStream in2 = urlEntity2.getContent();
+            String str2 = "";
+            while (true) {
+                int ch = in2.read();
+                if (ch == -1)
+                    break;
+                str2 += (char) ch;
+            }
+            
+//          JSONObject resultJson;
+//          try {
+//              resultJson = new JSONObject(str);
+//              String blobKey = resultJson.getString("blobKey");
+//              String servingUrl = resultJson.getString("servingUrl");
+//          } catch (JSONException e) {
+//              // TODO Auto-generated catch block
+//              e.printStackTrace();
+//          }
+            
+            Log.v("blobkey", str2);
+
+            
+        } 
+        catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } 
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        return null;
+    }
 
 }
---
+++ GAEBackend/travellog/guestbook/guestbook-war/src/main/java/com/google/appengine/demos/guestbook/GetUploadUrl.java
@@ -0,0 +1,28 @@
+package com.google.appengine.demos.guestbook;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+
+@SuppressWarnings("serial")
+public class GetUploadUrl extends HttpServlet {
+    BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+    
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String blobUploadUrl = blobstoreService.createUploadUrl("/blob/upload");
+        
+        resp.setStatus(HttpServletResponse.SC_OK);
+        resp.setContentType("text/plain");
+
+        PrintWriter out = resp.getWriter();
+        out.print(blobUploadUrl);
+        out.flush();
+        out.close();
+    }
+}
